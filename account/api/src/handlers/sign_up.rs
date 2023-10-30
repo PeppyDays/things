bb5148792bd9660240d8a1e378523140c09e127f
Arine <peppydays@gmail.com>
2023-10-31 01:54:51 +0900

    Modified sign up flow by adding registering identity

--- account/api/src/handlers/sign_up.rs
+++ account/api/src/handlers/sign_up.rs
@@ -3,9 +3,13 @@ use axum::{extract::State, Json};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-use crate::{container::Container, errors::Error};
+use domain::identity::commands::Command as IdentityCommand;
+use domain::identity::errors::Error as IdentityError;
+use domain::identity::models::Role as IdentityRole;
 use domain::user::commands::Command as UserCommand;
 use domain::user::errors::Error as UserError;
+
+use crate::{container::Container, errors::Error};
 
 #[derive(Deserialize)]
 pub struct Request {
@@ -25,6 +29,34 @@ pub async fn sign_up_with_credential(
     Json(request): Json<Request>,
 ) -> Result<Json<Response>, Error> {
     let id = Uuid::new_v4();
+
+    register_user(&mut container, request, id).await?;
+    register_identity(&mut container, id).await?;
+
+    Ok(Json(Response { id }))
+}
+
+async fn register_identity(container: &mut Container, id: Uuid) -> Result<(), Error> {
+    let command = IdentityCommand::RegisterIdentity {
+        id,
+        role: IdentityRole::Member,
+    };
+
+    container
+        .identity_command_executor
+        .execute(command)
+        .await
+        .map_err(|error| match error {
+            IdentityError::AlreadyRegistered { .. } => {
+                Error::new(StatusCode::CONFLICT, error.to_string())
+            }
+            _ => Error::new(StatusCode::INTERNAL_SERVER_ERROR, error.to_string()),
+        })?;
+
+    Ok(())
+}
+
+async fn register_user(container: &mut Container, request: Request, id: Uuid) -> Result<(), Error> {
     let command = UserCommand::RegisterUser {
         id,
         name: request.name,
@@ -37,11 +69,12 @@ pub async fn sign_up_with_credential(
         .user_command_executor
         .execute(command)
         .await
-        .map(|_| Json(Response { id }))
         .map_err(|error| match error {
             UserError::AlreadyRegistered { .. } => {
                 Error::new(StatusCode::CONFLICT, error.to_string())
             }
             _ => Error::new(StatusCode::INTERNAL_SERVER_ERROR, error.to_string()),
-        })
+        })?;
+
+    Ok(())
 }
