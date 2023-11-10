6d3828b9fd47c9168b78055998f91228e069fe12
Arine <peppydays@gmail.com>
2023-11-11 01:55:15 +0900

    Loaded configuration for running apps from environment variables
    - Considered to use `dotenvy`, but it would enough just using `std::env`

--- account/api/src/main.rs
+++ account/api/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::net::{Ipv4Addr, SocketAddr};
 
 use api::{container::get_container, router};
@@ -7,8 +8,12 @@ use axum::Server;
 async fn main() {
     let container = get_container().await;
     let app = router::create_router(container);
+    let port = match env::var("ACCOUNT_APPLICATION_PORT") {
+        Ok(p) => p.parse::<u16>().unwrap(),
+        Err(_) => 8080,
+    };
 
-    Server::bind(&SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8080)))
+    Server::bind(&SocketAddr::from((Ipv4Addr::LOCALHOST, port)))
         .serve(app.into_make_service())
         .await
         .unwrap();
