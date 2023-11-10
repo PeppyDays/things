9bb30b51a02f4f45360c973411a3435a875520c4
Arine <peppydays@gmail.com>
2023-11-11 01:55:15 +0900

    Loaded configuration for running apps from environment variables
    - Considered to use `dotenvy`, but it would enough just using `std::env`

--- account/api/src/container.rs
+++ account/api/src/container.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use sqlx::mysql::MySqlPoolOptions;
 
 use domain::identity::services::Service as IdentityService;
@@ -16,10 +18,12 @@ pub struct Container {
 
 impl Container {
     pub async fn new() -> Self {
+        let database_url = get_database_url();
+
         let user_repository = UserMySqlRepository::new(
             MySqlPoolOptions::new()
                 .max_connections(5)
-                .connect("mysql://root:welcome@localhost:3306/account")
+                .connect(&database_url)
                 .await
                 .unwrap(),
         );
@@ -29,7 +33,7 @@ impl Container {
         let identity_repository = IdentityMySqlRepository::new(
             MySqlPoolOptions::new()
                 .max_connections(5)
-                .connect("mysql://root:welcome@localhost:3306/account")
+                .connect(&database_url)
                 .await
                 .unwrap(),
         );
@@ -45,4 +49,20 @@ impl Container {
 
 pub async fn get_container() -> Container {
     Container::new().await
+}
+
+fn get_database_url() -> String {
+    let host = env::var("ACCOUNT_DATABASE_HOST").unwrap_or(String::from("127.0.0.1"));
+    let port = env::var("ACCOUNT_DATABASE_PORT")
+        .unwrap_or(String::from("3306"))
+        .parse::<u16>()
+        .unwrap();
+    let username = env::var("ACCOUNT_DATABASE_USERNAME").unwrap_or(String::from("root"));
+    let password = env::var("ACCOUNT_DATABASE_PASSWORD").unwrap_or(String::from("welcome"));
+    let schema = env::var("ACCOUNT_DATABASE_SCHEMA").unwrap_or(String::from("account"));
+
+    format!(
+        "mysql://{}:{}@{}:{}/{}",
+        username, password, host, port, schema
+    )
 }
